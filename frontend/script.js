1d946cd9a5956b45010b5fd2f0418528cf9425fa
Yb-M <ybermarlon@gmail.com>
2023-05-24 00:33:32 -0500

    v1.1-BETA Se adapto los estilos y se logró arreglar el problema con el botón de iniciar sesión, más no con el botón de cerrar

--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,15 @@
 // * Abrir y Cerrar Los Formularios Login y Register
 const evolutara = document.querySelector('.evolutara');
 const linkLogin = document.querySelector('.entrar-login');
-const linkRegister = document.querySelector('.entrar-registrar');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
-linkRegister.addEventListener('click', () => {
-    evolutara.classList.add('active');
+btnPopup.addEventListener('click', () => {
+    evolutara.classList.add('active-popup');
 });
 
 linkLogin.addEventListener('click', () => {
     evolutara.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', () => {
-    evolutara.classList.add('active-popup');
 });
 
 iconClose.addEventListener('click', () => {
@@ -26,13 +21,13 @@ function validate() {
     let user = document.getElementById('input-correo').value;
     let clave = document.getElementById('input-pass').value;
 
-    if (user == "ybermarlon@gmail.com" && clave == "123") {
+    if (user == "admin@ucvvirtual.edu.pe" && clave == "123") {
         alert("Se Ingresó Como Administrador");
         window.location = "./admin/index.html";
 
         return false;
     }
-    if (user == "user@gmail.com" && clave == "12") {
+    if (user == "user@ucvvirtual.edu.pe0" && clave == "123") {
         window.location = "./cliente/catalogo.html";
         return false;
     } else {
