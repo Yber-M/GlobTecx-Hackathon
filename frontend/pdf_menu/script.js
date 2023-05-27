ffd79097428007dcacb8b089f8bc5a2bdf6698c1
ApoTheddy <apotheddy@hotmail.com>
2023-05-27 17:39:06 -0500

    Modificando el codigo del visor de pdf

--- frontend/pdf_menu/script.js
+++ frontend/pdf_menu/script.js
@@ -1,2 +1,36 @@
-var textoElemento = document.getElementById("h2#jojo");
-  textoElemento.style.display = "none";
+// Obtener referencias a los elementos HTML
+const pdfFileInput = document.getElementById("pdf-file");
+const pdfEmbed = document.getElementById("pdf-embed");
+const messagesContainer = document.getElementById("messages");
+const inputMessage = document.getElementById("input-message");
+
+// Manejar la carga de un archivo PDF
+pdfFileInput.addEventListener("change", async function (event) {
+  const file = event.target.files[0];
+
+  // Enviar el pdf a la base de datos
+  let response = await axios.get("http://localhost:3000/files/");
+
+  const fileURL = URL.createObjectURL(file);
+
+  pdfEmbed.setAttribute("src", fileURL);
+});
+
+// Manejar el envío de un mensaje
+inputMessage.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    const message = inputMessage.value;
+    if (message.trim() !== "") {
+      addMessage("Tú", message);
+      inputMessage.value = "";
+    }
+  }
+});
+
+// Función para agregar un mensaje al chat
+function addMessage(author, text) {
+  const messageElement = document.createElement("div");
+  messageElement.innerHTML = `<strong>${author}:</strong> ${text}`;
+  messagesContainer.appendChild(messageElement);
+  messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
